0939a4e6548461eb031b69572c4d5adf9220690b
Tupi Guedes <tupiguedes@outlook.com>
2021-08-18 11:38:01 -0300

    Showing the department table data on the screen

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,37 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Connection conn = DB.getConnection();
-		
-		if (conn != null) {
-			System.out.println("Success!");
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			
+			conn = DB.getConnection();
+			
+			stmt = conn.createStatement();
+			String sql = "select * from department";
+			
+			rs = stmt.executeQuery(sql);
+			
+			while (rs.next()) {
+				System.out.println(rs.getInt("Id") + " - " + rs.getString("Name"));
+			}
+			
+		} 
+		catch (SQLException e) {
+			System.out.println("DATA PRINTING ERROR >>> " + e.getMessage());
+		} 
+		finally {
+			DB.closeConnection(conn, stmt, rs); 
 		}
-		
-		DB.closeConnection(); 
 	}
 }
--- src/db/DB.java
+++ src/db/DB.java
@@ -4,7 +4,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
@@ -25,6 +27,17 @@ public class DB {
 		return conn;
 	}
 	
+	private static Properties loadProperties() {
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties prop = new Properties();
+			prop.load(fs);
+			return prop;
+		}
+		catch (IOException e) {
+			throw new DbException("ERROR CONNECTION cod.:02>>> " + e.getMessage());
+		}
+	}
+	
 	public static void closeConnection() {
 		if (conn != null) {
 			try {
@@ -36,14 +49,28 @@ public class DB {
 		}
 	}
 	
-	private static Properties loadProperties() {
-		try (FileInputStream fs = new FileInputStream("db.properties")) {
-			Properties prop = new Properties();
-			prop.load(fs);
-			return prop;
+	public static void closeConnection(Connection conn, Statement statement) {
+		if (conn != null && statement != null) {
+			try {
+				conn.close();
+				statement.close();
+			} 
+			catch (SQLException e) {
+				throw new DbException("ERROR CLOSE CONNECTION Statement cod.:03 >>>");
+			}
 		}
-		catch (IOException e) {
-			throw new DbException("ERROR CONNECTION cod.:02>>> " + e.getMessage());
+	}
+	
+	public static void closeConnection(Connection conn, Statement statement, ResultSet resultSet) {
+		if (conn != null && statement != null && resultSet != null) {
+			try {
+				conn.close();
+				statement.close();
+				resultSet.close();
+			} 
+			catch (SQLException e) {
+				throw new DbException("ERROR CLOSE CONNECTION Statement AND ResultSet cod.:03 >>>");
+			}
 		}
 	}
 }
