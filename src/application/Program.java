2e4c31a29aa21b6eb761f92d9364c9e79d5bd79e
Tupi Guedes <tupiguedes@outlook.com>
2021-08-19 15:22:34 -0300

    Updating seller table data on screen

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,9 @@
 package application;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -18,77 +14,46 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
+		
 		Connection conn = null;
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
-
+		
+		System.out.print("Enter the value you want to add: ");
+		double value = sc.nextDouble();
+		System.out.print("Enter the person's id: ");
+		int id = sc.nextInt();
+		
 		try {
-			System.out.print("Name: ");
-			String name = sc.nextLine();
-			System.out.print("Email: ");
-			String email = sc.nextLine();
-			System.out.print("BirthDate: ");
-			Date date = new java.sql.Date(sdf.parse(sc.nextLine()).getTime());
-			System.out.print("Base salary: ");
-			double baseSalary = sc.nextDouble();
-			System.out.print("Department number: ");
-			int departmentId = sc.nextInt();
-			
 			conn = DB.getConnection();
-
-			String sql = "INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId) "
-					+ "VALUES (?, ?, ?, ?, ?)";
-
-			stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-
-			stmt.setString(1, name);
-			stmt.setString(2, email);
-			stmt.setDate(3, date);
-			stmt.setDouble(4, baseSalary);
-			stmt.setInt(5, departmentId);
-
-			int rows = stmt.executeUpdate();
 			
-			verification(name, rows, stmt);
+			String sql = "UPDATE seller SET BaseSalary = BaseSalary + ? "
+					+ "WHERE "
+					+ "(Id = ?)";
 			
-			rs = stmt.executeQuery("SELECT * FROM seller");
+			stmt = conn.prepareStatement(sql);
+			stmt.setDouble(1, value);
+			stmt.setInt(2, id);
+			
+			stmt.executeUpdate();
+			
+			rs = stmt.executeQuery("SELECT * FROM seller WHERE (Id = " + id + ")");
+			
 			
 			while (rs.next()) {
-				System.out.println(rs.getString("Id") + " - " + rs.getString("Name") + " - " + rs.getString("Email")
-						+ " - " + rs.getDate("BirthDate") + " - " + rs.getDouble("BaseSalary") + " - "
-						+ rs.getInt("DepartmentId"));
+				System.out.println(rs.getString("Name") + " - " + rs.getDouble("BaseSalary"));
 			}
 		} 
 		catch (SQLException e) {
-			System.out.println("ERROR SQL cod.:304>>> " + e.getMessage());
-		} 
-		catch (ParseException e) {
-			System.out.println("ERROR cod.:305>>> " + e.getMessage());
+			System.out.println("ERROR cod.:304 >>>" + e.getMessage());
 		}
 		finally {
 			DB.closeConnection(conn, stmt, rs);
 		}
 		sc.close();
-
 	}
 
-	private static void verification(String name, int rows, PreparedStatement stmt) {
-		try {
-			if (rows > 0) {
-				ResultSet rs = stmt.getGeneratedKeys();
-				while (rs.next()) {
-					int id = rs.getInt(1);
-					System.out.println("\n" + name + ", been successfully added! ID=" + id);
-				}
-			}
-			else {
-				System.out.println("No rows affected");
-			}
-		}
-		catch (SQLException e) {
-			System.out.println("ERROR SQL cod.:306>>> " + e.getMessage());
-		}
-	}
+
+	
+	
 }
