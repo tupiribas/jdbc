74b62a42ff5e69bb944d8c38079247467bd02182
Tupi Guedes <tupiguedes@outlook.com>
2021-08-20 11:40:58 -0300

    Data transition from the seller table to the screen

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,71 +4,95 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Locale;
 import java.util.Scanner;
 
 import db.DB;
+import db.DbException;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		Connection conn = null;
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
-		
-		System.out.println("Enter the department identifier to delete: ");
+
+		System.out.println("Enter the department identifier: ");
 		int id = sc.nextInt();
-		
+		System.out.println("Enter the salary amount you want to add: ");
+		Double salaryAdd = sc.nextDouble();
+
 		try {
 			conn = DB.getConnection();
 			
-			String sql = "DELETE FROM department WHERE Id = ?";
+			conn.setAutoCommit(false);
+			
+			String sql = "UPDATE seller SET BaseSalary = BaseSalary + ? WHERE DepartmentId = ?";
 			stmt = conn.prepareStatement(sql);
 			
-			System.out.println("You are deleting the " + id + " department, do you want to do this?");
-			String resp = sc.next().toUpperCase();
+			stmt.setDouble(1, salaryAdd);
+			stmt.setInt(2, id);
 			
-			verification(id, resp, stmt);	
+//			Possible Exception!
+//			int x = 1;
+//			if (x < 2) {
+//				throw new SQLException("Fake error!");
+//			}
+			
+			System.out.println("You will make an addition to the ID=" + id 
+					+ " employee's R$" + String.format("%.2f", salaryAdd) 
+					+ ", do you want to continue? [Y/N]:");
+			String resp = sc.next().toUpperCase();
+			verification(id, resp, stmt, conn);
 			printDepartment(rs, stmt);
 		} 
 		catch (SQLException e) {
-			System.out.println("ERRO cod.:304 >>> " + e.getMessage());
-		}
+			try {
+				conn.rollback();
+				throw new DbException("ERRO cod.:308 >>> Transaction rolled back! Caused by: " + e.getMessage());
+			} 
+			catch (SQLException e1) {
+				throw new DbException("ERRO cod.:309 >>> Erro trying to rollback! Caused by: " + e1.getMessage());
+			}
+		} 
 		finally {
 			DB.closeConnection(conn, stmt, rs);
 		}
 		sc.close();
 	}
 
-	private static void printDepartment(ResultSet rs, PreparedStatement stmt) {
+	private static void printDepartment(ResultSet rs, Statement stmt) {
 		try {
-			rs = stmt.executeQuery("SELECT * FROM department");
-			
+			rs = stmt.executeQuery("SELECT * FROM seller");
+
 			while (rs.next()) {
-				System.out.println(rs.getInt("Id") + " - " + rs.getString("Name"));
+				System.out.println(rs.getInt("Id") 
+						+ " - " + rs.getString("Name") 
+						+ " - " + rs.getDouble("BaseSalary"));
 			}
 		} 
 		catch (SQLException e) {
 			System.out.println("ERRO cod.:306 >>> " + e.getMessage());
 		}
 	}
-
-	private static void verification(int id, String resp, PreparedStatement stmt) {
+	
+	private static void verification(int id, String resp, PreparedStatement stmt, Connection conn) {
 		try {
-			
-			if (resp.equals("S")) {
+			if (resp.equals("Y")) {
+				stmt.executeUpdate();
 				stmt.setInt(1, id);
 				int rowsAffected = stmt.executeUpdate();
+				conn.commit();
 				System.out.println("Done! Rows affected: " + rowsAffected);
 			}
-			
 		} 
 		catch (SQLException e) {
 			System.out.println("ERRO cod.:305 >>> " + e.getMessage());
 		}
 	}
-	
+
 }
