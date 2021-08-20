82753aaa2c4a4324395964d7798cabcc111aa426
Tupi Guedes <tupiguedes@outlook.com>
2021-08-19 22:20:32 -0300

    Deleting data from the seller's table on the screen

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,33 +19,23 @@ public class Program {
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
 		
-		System.out.print("Enter the value you want to add: ");
-		double value = sc.nextDouble();
-		System.out.print("Enter the person's id: ");
+		System.out.println("Enter the department identifier to delete: ");
 		int id = sc.nextInt();
 		
 		try {
 			conn = DB.getConnection();
 			
-			String sql = "UPDATE seller SET BaseSalary = BaseSalary + ? "
-					+ "WHERE "
-					+ "(Id = ?)";
-			
+			String sql = "DELETE FROM department WHERE Id = ?";
 			stmt = conn.prepareStatement(sql);
-			stmt.setDouble(1, value);
-			stmt.setInt(2, id);
 			
-			stmt.executeUpdate();
+			System.out.println("You are deleting the " + id + " department, do you want to do this?");
+			String resp = sc.next().toUpperCase();
 			
-			rs = stmt.executeQuery("SELECT * FROM seller WHERE (Id = " + id + ")");
-			
-			
-			while (rs.next()) {
-				System.out.println(rs.getString("Name") + " - " + rs.getDouble("BaseSalary"));
-			}
+			verification(id, resp, stmt);	
+			printDepartment(rs, stmt);
 		} 
 		catch (SQLException e) {
-			System.out.println("ERROR cod.:304 >>>" + e.getMessage());
+			System.out.println("ERRO cod.:304 >>> " + e.getMessage());
 		}
 		finally {
 			DB.closeConnection(conn, stmt, rs);
@@ -53,7 +43,32 @@ public class Program {
 		sc.close();
 	}
 
+	private static void printDepartment(ResultSet rs, PreparedStatement stmt) {
+		try {
+			rs = stmt.executeQuery("SELECT * FROM department");
+			
+			while (rs.next()) {
+				System.out.println(rs.getInt("Id") + " - " + rs.getString("Name"));
+			}
+		} 
+		catch (SQLException e) {
+			System.out.println("ERRO cod.:306 >>> " + e.getMessage());
+		}
+	}
 
-	
+	private static void verification(int id, String resp, PreparedStatement stmt) {
+		try {
+			
+			if (resp.equals("S")) {
+				stmt.setInt(1, id);
+				int rowsAffected = stmt.executeUpdate();
+				System.out.println("Done! Rows affected: " + rowsAffected);
+			}
+			
+		} 
+		catch (SQLException e) {
+			System.out.println("ERRO cod.:305 >>> " + e.getMessage());
+		}
+	}
 	
 }
